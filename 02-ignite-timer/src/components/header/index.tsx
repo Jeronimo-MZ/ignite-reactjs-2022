17702077e0ca1d8e2e1abcf93840a776d4e7b19e
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-20 18:08:05 +0200

    feat(ignite-timer): add Header

--- 02-ignite-timer/src/components/header/index.tsx
+++ 02-ignite-timer/src/components/header/index.tsx
@@ -1,3 +1,24 @@
+import { Scroll, Timer } from "phosphor-react";
+import { Link, NavLink } from "react-router-dom";
+
+import logo from "@/assets/logo.svg";
+
+import { HeaderContainer } from "./styles";
+
 export function Header() {
-  return <header>Header</header>;
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <img src={logo} alt="" />
+      </Link>
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
 }
