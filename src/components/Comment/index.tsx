8c493c750c2d69374fb3153ebbd9d5dfb1d16bb0
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-20 02:32:43 +0200

    feat: handle likes

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,11 +1,13 @@
 import styles from "./comment.module.css";
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "../Avatar";
+import { useState } from "react";
 type CommentProps = {
     content: string;
     onDelete: () => void;
 };
 export const Comment = ({ content, onDelete }: CommentProps) => {
+    const [likeCount, setLikeCount] = useState(0);
     return (
         <div className={styles.comment}>
             <Avatar
@@ -29,9 +31,9 @@ export const Comment = ({ content, onDelete }: CommentProps) => {
                     <main>{content}</main>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={() => setLikeCount((prev) => prev + 1)}>
                         <ThumbsUp />
-                        Aplaudir<span>33</span>
+                        Aplaudir<span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
