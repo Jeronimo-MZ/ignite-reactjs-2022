839fe4f35726a9d2e918c165f0cdd7691a7815bd
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-20 17:26:48 +0200

    feat(ignite-timer): add default layout

--- 02-ignite-timer/src/router.tsx
+++ 02-ignite-timer/src/router.tsx
@@ -3,10 +3,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { History } from "@/pages/history";
 import { Home } from "@/pages/home";
 
+import { DefaultLayout } from "./layouts/default";
+
 export function Router() {
   const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/history", element: <History /> },
+    {
+      path: "/",
+      element: <DefaultLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/history", element: <History /> },
+      ],
+    },
   ]);
   return <RouterProvider router={router} />;
 }
