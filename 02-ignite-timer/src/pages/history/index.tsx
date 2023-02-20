265d0e20bba094d667cdb565db41c2f7684249e1
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-20 19:07:28 +0200

    feat(ignite-timer): add History page

--- 02-ignite-timer/src/pages/history/index.tsx
+++ 02-ignite-timer/src/pages/history/index.tsx
@@ -1,3 +1,31 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
 export function History() {
-  return <h1>History</h1>;
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[1, 2, 3, 4, 5, 6].map(item => (
+              <tr key={item}>
+                <td>Tarefa 1</td>
+                <td>25 minutos</td>
+                <td>há 2 horas</td>
+                <td>Concluído</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
