031a8f62009a3e48be5a076717e4cbb6506ebb73
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-22 16:19:37 +0200

    feat(dtmoney): add transactions table

--- 03-dt-money/src/pages/transactions/index.tsx
+++ 03-dt-money/src/pages/transactions/index.tsx
@@ -1,12 +1,34 @@
 import { Summary } from "@/components/summary";
-import { Header } from "../../components/header";
-import { TransactionsContainer } from "./styles";
+import { Header } from "@/components/header";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
-    <TransactionsContainer>
+    <>
       <Header />
       <Summary />
-    </TransactionsContainer>
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/02/2023</td>
+            </tr>
+            <tr>
+              <td>Hambúrguer</td>
+              <td>
+                <PriceHighlight variant="outcome"> - R$ 59,90 </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>14/02/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </>
   );
 }
