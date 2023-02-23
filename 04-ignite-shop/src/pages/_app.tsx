ede4e7235a1cb1cb949690de59eac3bbf2f8efb8
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-23 14:47:48 +0200

    feat(ignite-shop): add header

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import type { AppProps } from "next/app";
 
 import { globalStyles } from "@/styles/global";
+import logoImg from "@/assets/logo.svg";
+import Image from "next/image";
+import { AppContainer, Header } from "@/styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <AppContainer>
+            <Header>
+                <Image src={logoImg} alt="" />
+            </Header>
+            <Component {...pageProps} />
+        </AppContainer>
+    );
 }
