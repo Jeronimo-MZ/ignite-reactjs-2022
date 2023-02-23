e5f11615230d729824ac4367f26f0b9204ef9192
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-23 18:04:48 +0200

    feat(ignite-shop): add Home page

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,17 +1,11 @@
 import { styled } from "@/styles";
+import { HomeContainer, Product } from "@/styles/pages/Home";
 import Head from "next/head";
-
-const Button = styled("button", {
-    backgroundColor: "$green300",
-    border: 0,
-    borderRadius: 4,
-    color: "white",
-    padding: "0.5rem 1rem",
-    cursor: "pointer",
-    "&:hover": {
-        filter: "brightness(.8)",
-    },
-});
+import Image from "next/image";
+import shirt1 from "@/assets/shirts/1.png";
+import shirt2 from "@/assets/shirts/2.png";
+import shirt3 from "@/assets/shirts/3.png";
+import shirt4 from "@/assets/shirts/3.png";
 
 export default function Home() {
     return (
@@ -19,11 +13,36 @@ export default function Home() {
             <Head>
                 <title>Ignite Shop</title>
             </Head>
-            <main>
-                Hello Ignite Shop
-                <br />
-                <Button>Click</Button>
-            </main>
+            <HomeContainer>
+                <Product>
+                    <Image src={shirt1} width={520} height={480} alt="" />
+                    <footer>
+                        <strong>Camiseta 1</strong>
+                        <span>R$ 79,10</span>
+                    </footer>
+                </Product>
+                <Product>
+                    <Image src={shirt2} width={520} height={480} alt="" />
+                    <footer>
+                        <strong>Camiseta 2</strong>
+                        <span>R$ 79,20</span>
+                    </footer>
+                </Product>
+                {/* <Product>
+                    <Image src={shirt3} width={520} height={480} alt="" />
+                    <footer>
+                        <strong>Camiseta 3</strong>
+                        <span>R$ 79,30</span>
+                    </footer>
+                </Product>
+                <Product>
+                    <Image src={shirt4} width={520} height={480} alt="" />
+                    <footer>
+                        <strong>Camiseta 4</strong>
+                        <span>R$ 79,40</span>
+                    </footer>
+                </Product> */}
+            </HomeContainer>
         </>
     );
 }
