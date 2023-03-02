db6d0b19466ec6175442e27bce52991dd78070b5
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-03-02 11:29:30 +0200

    feat(design-system): add Checkbox component

---
+++ 05-design-system/packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,14 @@
+import { Check } from "phosphor-react";
+import { ComponentProps } from "@stitches/react";
+import { CheckboxContainer, CheckboxIndicator } from "./styles";
+
+export type CheckboxProps = ComponentProps<typeof CheckboxContainer>;
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  );
+}
